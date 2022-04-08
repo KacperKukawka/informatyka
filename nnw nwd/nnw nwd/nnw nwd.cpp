348104b08a9c2bb14b41395a486939f19c2c0d2a
Kacper Kukawka <kacper.kukawka@gmail.com>
2022-04-08 09:17:31 +0200

    Komentarz

---
+++ nnw nwd/nnw nwd/nnw nwd.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{	//NWD
+	int a,b;
+	int iloczyn;
+	int NWW;
+	cout << "Podaj a: ";
+	cin >> a;
+	cout << "Podaj b: ";
+	cin >> b;
+	cout << "NWD(" << a << "," << b << ") = ";
+	iloczyn = a * b;
+	while (a != b)
+	{
+		if (a < b) 
+			b = b - a;
+		else
+			a = a - b;
+	}
+	NWW = iloczyn / a;
+	cout << a << endl;
+	cout << NWW << endl;
+	return 0; 
+} 
