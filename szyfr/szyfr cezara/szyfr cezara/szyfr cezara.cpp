ded86136bcf19990979a91cf474a7a62ad97cec3
Kacper Kukawka <kacper.kukawka@gmail.com>
2022-04-21 10:11:41 +0200

    Komentarz

---
+++ szyfr/szyfr cezara/szyfr cezara/szyfr cezara.cpp
@@ -0,0 +1,26 @@
+﻿// a b c d e f g h i j k l m n o p q r s t u v w x y z
+
+#include <iostream>
+#include <cstring>
+int main()
+{
+	int klucz;
+	cout << "podaj klucz: ";
+	cin >> klucz;
+
+	char tab[1000];
+
+	dl = 1000(tab),
+
+	!(klucz = -26 and klucz = 26),
+	exit
+
+		if klucz >= 0, {
+			i < dl
+			if tab + klucz <= z{
+				get tab += klucz
+}
+		}
+
+}
+
