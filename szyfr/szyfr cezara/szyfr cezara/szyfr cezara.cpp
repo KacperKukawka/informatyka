237a79748514faf86cdf0a5c1db7b930bf1314b9
Kacper Kukawka <kacper.kukawka@gmail.com>
2022-04-22 09:15:01 +0200

    Komentarz

--- szyfr/szyfr cezara/szyfr cezara/szyfr cezara.cpp
+++ szyfr/szyfr cezara/szyfr cezara/szyfr cezara.cpp
@@ -1,26 +1,60 @@
 ﻿// a b c d e f g h i j k l m n o p q r s t u v w x y z
 
+
 #include <iostream>
 #include <cstring>
+using namespace std;
+
+void funkcja(char tab[], int klucz);
+
 int main()
 {
+	
 	int klucz;
+
 	cout << "podaj klucz: ";
 	cin >> klucz;
 
 	char tab[1000];
+	cout << "podaj wyraz: ";
+	cin >> tab;
 
-	dl = 1000(tab),
-
-	!(klucz = -26 and klucz = 26),
-	exit
-
-		if klucz >= 0, {
-			i < dl
-			if tab + klucz <= z{
-				get tab += klucz
-}
-		}
-
+	
 }
 
+	void funkcja(char tab[], int klucz) {
+
+		int dl;
+		dl = strlen(tab);			//k1
+	
+			if (!(klucz >= -26 && klucz <= 26)) {  //k2
+				return;
+			}
+			if (klucz >= 0) {			//k3
+	
+				for(int i = 0; i < 10; i++){
+					i < dl					//k4
+				}	
+				
+				if tab + klucz <= z{		//k5
+					 tab += klucz
+					else {
+						tab < tab + klucz -26
+					}
+				
+				}
+				else{
+					if klucz >= 0, {			
+
+				i < dl					
+				if tab + klucz <= z{
+					 tab += klucz
+					else {
+						tab < tab + klucz - 26
+					}
+
+				}
+			}
+		
+
+	}
