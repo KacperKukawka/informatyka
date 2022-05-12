16eda2da1794eec3ab2837a17f609b6f4f089699
Kacper Kukawka <kacper.kukawka@gmail.com>
2022-05-12 10:07:20 +0200

    Komentarz

---
+++ wyszukiwanie binarne/wyszukiwanie_binarne_kkukawka/wyszukiwanie_binarne_kkukawka/wyszukiwanie_binarne_kkukawka.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void funkcja(char tab[], int szukana);
+
+int main()
+{
+	
+	int szukana;
+	
+	char tab[1000];
+	cout << "podaj szukana: ";
+	cin >> tab;
+
+	
+}
+
+void funkcja(char tab[], int szukana) {
+int l, p, sr;
+int pozycja;
+
+l < 0
+	p < 15
+	sr < (l+p)/2
+
+		if (l <= p) {
+
+		}
+}
